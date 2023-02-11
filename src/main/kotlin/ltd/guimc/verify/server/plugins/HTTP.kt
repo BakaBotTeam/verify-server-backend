2aa956f3e3dc6907e79854fdb532112b9b68036e
xiluo <xiluo@guimc.ltd>
2023-02-11 12:07:34 +0800

    fix

--- src/main/kotlin/ltd/guimc/verify/server/plugins/HTTP.kt
+++ src/main/kotlin/ltd/guimc/verify/server/plugins/HTTP.kt
@@ -6,15 +6,15 @@ import io.ktor.server.plugins.defaultheaders.*
 import io.ktor.server.application.*
 
 fun Application.configureHTTP() {
-    install(HttpsRedirect) {
-            // The port to redirect to. By default 443, the default HTTPS port.
-            sslPort = 443
-            // 301 Moved Permanently, or 302 Found redirect.
-            permanentRedirect = true
-        }
-    install(HSTS) {
-        includeSubDomains = true
-    }
+    // install(HttpsRedirect) {
+    //         // The port to redirect to. By default 443, the default HTTPS port.
+    //         sslPort = 443
+    //         // 301 Moved Permanently, or 302 Found redirect.
+    //         permanentRedirect = true
+    //     }
+    // install(HSTS) {
+    //     includeSubDomains = true
+    // }
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
     }
